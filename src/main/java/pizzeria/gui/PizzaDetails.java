8988884db24efc7d838dfeb2ece633bb3530e153
88mary256 <mary.jlm.2006@gmail.com>
2019-03-12 19:29:23 -0400

    D11: Fixed Checkstyle. Added missing javadocs.

--- src/main/java/pizzeria/gui/PizzaDetails.java
+++ src/main/java/pizzeria/gui/PizzaDetails.java
@@ -20,17 +20,35 @@ import javax.swing.border.EmptyBorder;
 
 import pizzeria.data.IngredientType;
 
+/**
+ * Dialog to set extra ingredients and ingredients to be removed.
+ *
+ * @author Marines Lopez Soliz
+ */
 public class PizzaDetails extends JDialog {
 
+    /** This dialog. **/
     private final JDialog me = this;
+
+    /** Main Panel. **/
     private final JPanel contentPanel = new JPanel();
-    final JPanel extraIngredientsPanel = new JPanel();
-    final JPanel toRemoveIngredientsPanel = new JPanel();
-    final JComboBox toAddCombobox = new JComboBox();
-    final JComboBox toRemoveCombobox = new JComboBox();
+
+    /** Panel for extra ingredients. **/
+    private final JPanel extraIngredientsPanel = new JPanel();
+
+    /** Panel for ingredients to be removed. **/
+    private final JPanel toRemoveIngredientsPanel = new JPanel();
+
+    /** Combobox for add ingredient. **/
+    private final JComboBox toAddCombobox = new JComboBox();
+
+    /** Combobox for to be removed ingredients. **/
+    private final JComboBox toRemoveCombobox = new JComboBox();
 
     /**
      * Launch the application.
+     *
+     * @param args arguments to run.
      */
     public static void main(final String[] args) {
         try {
@@ -51,20 +69,19 @@ public class PizzaDetails extends JDialog {
         getContentPane().setLayout(new BorderLayout());
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(contentPanel, BorderLayout.CENTER);
-        {
-            final JButton addButton = new JButton("+");
-            addButton.setBounds(204, 11, 41, 23);
-            addButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(final ActionEvent arg0) {
-                    new IngredientPanel(
-                            (IngredientType) toAddCombobox.getSelectedItem(),
-                            extraIngredientsPanel);
-                }
-            });
-            contentPanel.setLayout(null);
-            contentPanel.add(addButton);
-        }
+
+        final JButton addButton = new JButton("+");
+        addButton.setBounds(204, 11, 41, 23);
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                new IngredientPanel(
+                        (IngredientType) toAddCombobox.getSelectedItem(),
+                        extraIngredientsPanel);
+            }
+        });
+        contentPanel.setLayout(null);
+        contentPanel.add(addButton);
 
         final JLabel lblExtraIngredients = new JLabel(
                 "Ingredients to be added");
@@ -118,47 +135,61 @@ public class PizzaDetails extends JDialog {
         toRemoveCombobox.setSelectedIndex(0);
         toRemoveCombobox.setBounds(255, 163, 160, 20);
         contentPanel.add(toRemoveCombobox);
-        {
-            final JPanel buttonPane = new JPanel();
-            buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-            getContentPane().add(buttonPane, BorderLayout.SOUTH);
-            {
-                final JButton okButton = new JButton("Save");
-                okButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-                        System.out.println("extra ingredients ");
-                        System.out.println(getExtraIngredientList());
-                        System.out.println("to remove ingredients");
-                        System.out.println(getToRemoveIngredientList());
-                    }
-                });
-                okButton.setActionCommand("OK");
-                buttonPane.add(okButton);
-                getRootPane().setDefaultButton(okButton);
+
+        final JPanel buttonPane = new JPanel();
+        buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+        final JButton okButton = new JButton("Save");
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                System.out.println("extra ingredients ");
+                System.out.println(getExtraIngredientList());
+                System.out.println("to remove ingredients");
+                System.out.println(getToRemoveIngredientList());
             }
-            {
-                final JButton cancelButton = new JButton("Cancel");
-                cancelButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-                        me.dispose();
-                    }
-                });
-                cancelButton.setActionCommand("Cancel");
-                buttonPane.add(cancelButton);
+        });
+        okButton.setActionCommand("OK");
+        buttonPane.add(okButton);
+        getRootPane().setDefaultButton(okButton);
+
+        final JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                me.dispose();
             }
-        }
+        });
+        cancelButton.setActionCommand("Cancel");
+        buttonPane.add(cancelButton);
+
     }
 
+    /**
+     * Getter of extra ingredient list.
+     *
+     * @return IngredientType list
+     */
     public List<IngredientType> getExtraIngredientList() {
         return getIngredientTypeList(extraIngredientsPanel);
     }
 
+    /**
+     * Getter of ingredient list to remove.
+     *
+     * @return IngredientType list
+     */
     public List<IngredientType> getToRemoveIngredientList() {
         return getIngredientTypeList(toRemoveIngredientsPanel);
     }
 
+    /**
+     * Get IngredientType list from panel.
+     *
+     * @param panel JPanel
+     * @return IngredientType list
+     */
     private List<IngredientType> getIngredientTypeList(final JPanel panel) {
         final List<IngredientType> result = new ArrayList<IngredientType>();
         for (final Component component : panel.getComponents()) {
